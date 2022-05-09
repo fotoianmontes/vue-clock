a9fda2eca63cc728c3cd60a7782c26fd124c1a0b
Ian Montes <fotoianmontes@gmail.com>
2022-05-09 20:59:42 +0100

    Clock function created

---
+++ src/components/ClockComponent.vue
@@ -0,0 +1,24 @@
+<script setup>
+let t = 0;
+let actualTime = 0;
+
+function actualizar() {
+
+ function Clock() {
+    actualTime = new Date();
+    t = actualTime.getHours() + ":" + actualTime.getMinutes() + ":" + actualTime.getSeconds();
+    console.log(t)
+     }
+    setInterval(Clock, 1000);
+}
+actualizar();
+
+</script>
+
+<template>
+    <p>{{t}}</p>
+</template>
+
+<style>
+
+</style>
